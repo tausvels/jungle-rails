b34ca002525eb3dd99b198631b134b4817af5ca8
tausvels <engr.tausifkhan@gmail.com>
2020-02-06 18:07:40 -0800

    created the model for users, controller for users, the new.html.erb file so users can signup(POST)route, added the def for new and create in user_controller, and ran bin/rake db:migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
+
   get "/about" => "about#show"
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
