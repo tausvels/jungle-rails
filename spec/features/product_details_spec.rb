9377112af02e0084764a1ccd15ed15e5ad91df80
tausvels <engr.tausifkhan@gmail.com>
2020-02-10 23:41:39 -0800

    implemented the product_detail feature testing

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: false do
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
 
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
 
-    10.times do |n|
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(4),
@@ -15,7 +14,6 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: false do
         price: 64.99
       )
     end
-  end
 
   scenario "They see all products" do
     # ACT
@@ -25,6 +23,8 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: false do
     # save_screenshot
 
     # VERIFY
-    expect(page).to have_css 'article.product', count: 10
+    find("a.btn-default").click
+    expect(page).to have_content("Description")
+
   end
 end
