ba83918311588470ed0104bc76bda5d10a7fc202
tausvels <engr.tausifkhan@gmail.com>
2020-02-10 17:37:49 -0800

    passed test while creating new user giving all values

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
+require "user.rb"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    subject {
+      described_class.new(name: "tausif", email: "abc@gmail.com", password: "abc", password_confirmation: "abc")
+    }
+    describe "validations" do
+      it "user is valid" do
+        # user = User.new(
+        #   name: "tausif",
+        #   email: "abc@gmail.com",
+        #   password: "abc",
+        #   password_confirmation: "abc"
+        # )
+        expect(subject).to be_valid
+      end
+    end
+
 end
